49709ff80f0facf56070ecf4eeca3f851e92de52
ericterpstra <eterps@gmail.com>
2013-09-02 22:55:25 -0500

    Added tags. Removed some WP specific styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,17 +7,29 @@ module.exports = function(grunt) {
         },
         buildType: 'Build',
         pkg: grunt.file.readJSON('package.json'),
+
         sass: {
             admin: {
+                options : {
+                    sourcemap: true
+                },
                 files: {
                     '<%= paths.css %>/style.css': '<%= paths.scss %>/style.scss',
                     '<%= paths.css %>/ie.css': '<%= paths.scss %>/ie.scss'
                 }
             }
+        },
+
+        watch: {
+            sass: {
+                files: './scss/**/*.scss',
+                tasks: ['sass:admin']
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['sass:admin']);
 };
